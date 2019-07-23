a7a8946180f74e92a7eb1205604771403e1c446c
ricardomarzochi <ricardomarzochi@gmail.com>
2019-07-23 20:34:17 +0200

    “newVersion-002“

---
+++ src/main/java/com/packsendme/lib/common/constants/PaymentConstants.java
@@ -0,0 +1,11 @@
+package com.packsendme.lib.common.constants;
+
+public final class PaymentConstants {
+	
+	public static final String CARD_PAY = "CREDITCARD_PAY";
+	public static final String VOUCHER_PAY = "VOUCHER_PAY";
+	public static final String PROMOTION_PAY = "PROMOTION_PAY";
+	public static final String STATUS_PAY_ACTIVE = "ACTIVATED";
+	public static final String STATUS_PAY_DISABLED = "DISABLED";
+
+}
