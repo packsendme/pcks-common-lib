9538b5b9328f4afb6ce3cee97494bf0cd39a0bb7
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-04-08 20:59:05 +0200

    NewVersion/0.2.0

---
+++ src/main/java/com/packsendme/lib/common/constants/BRE_SA_Constants.java
@@ -0,0 +1,12 @@
+package com.packsendme.lib.common.constants;
+
+public class BRE_SA_Constants {
+	
+	public static final String ROADWAY_BRE_SA_CACHE = "RoadwayBRE_SA";
+	public static final String AIRWAY_BRE_SA_CACHE = "AirwayBRE_SA";
+	public static final String MARITIMEWAY_BRE_SA_CACHE = "MaritimewayBRE_SA";
+	public static final String EXECUTE_BRE_SA_CACHE = "ExecuteBRE_SA";
+	
+
+
+}
