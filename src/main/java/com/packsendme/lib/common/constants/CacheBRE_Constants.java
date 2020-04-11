ac6fff6e40049f15c4e5c62a7151c72e45f1bfdb
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-04-11 23:07:06 +0200

    NewVersiom/0.2.0

---
+++ src/main/java/com/packsendme/lib/common/constants/CacheBRE_Constants.java
@@ -0,0 +1,28 @@
+package com.packsendme.lib.common.constants;
+
+public class CacheBRE_Constants {
+	
+	// South_America
+	public static final String ROADWAY_BRE_SA_CACHE = "RoadwayBRE_SA";
+	public static final String AIRWAY_BRE_SA_CACHE = "AirwayBRE_SA";
+	public static final String MARITIMEWAY_BRE_SA_CACHE = "MaritimewayBRE_SA";
+	public static final String EXECUTE_BRE_SA_CACHE = "ExecuteBRE_SA";
+	public static final String FUEL_BRE_SA_CACHE = "FuelBRE_SA";
+	public static final String TOLLS_BRE_SA_CACHE = "TollsBRE_SA";
+
+	// NORTH_America
+	public static final String ROADWAY_BRE_NA_CACHE = "RoadwayBRE_NA";
+	public static final String AIRWAY_BRE_NA_CACHE = "AirwayBRE_NA";
+	public static final String MARITIMEWAY_BRE_NA_CACHE = "MaritimewayBRE_NA";
+	public static final String EXECUTE_BRE_NA_CACHE = "ExecuteBRE_NA";
+	public static final String FUEL_BRE_NA_CACHE = "FuelBRE_NA";
+	public static final String TOLLS_BRE_NA_CACHE = "TollsBRE_NA";
+
+	// EUROPE
+	public static final String ROADWAY_BRE_EURO_CACHE = "RoadwayBRE_EURO";
+	public static final String AIRWAY_BRE_EURO_CACHE = "AirwayBRE_EURO";
+	public static final String MARITIMEWAY_BRE_EURO_CACHE = "MaritimewayBRE_EURO";
+	public static final String EXECUTE_BRE_EURO_CACHE = "ExecuteBRE_EURO";
+	public static final String FUEL_BRE_EURO_CACHE = "FuelBRE_EURO";
+	public static final String TOLLS_BRE_EURO_CACHE = "TollsBRE_EURO";
+}
