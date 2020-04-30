9c624e85572cdbd36724d30b3e36a906c6a9f517
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-04-30 11:24:39 +0200

    NewVersion/0.2.0

---
+++ src/main/java/com/packsendme/lib/utility/LocaleCountry.java
@@ -0,0 +1,23 @@
+package com.packsendme.lib.utility;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class LocaleCountry {
+	
+	public Map<String,String> locateList(String country) {
+	    Locale locales[] = SimpleDateFormat.getAvailableLocales();
+	    Map<String,String> countryLocateL = new HashMap<String,String>();
+	    
+	    for (int i = 0; i < locales.length; i++) {
+	     	if(locales[i].getDisplayCountry().equals(country)) {
+	     		countryLocateL.put(locales[i].getLanguage(), locales[i].getCountry());
+	     		return countryLocateL;
+	     	}
+	    }
+	    return countryLocateL;
+	}
+
+}
