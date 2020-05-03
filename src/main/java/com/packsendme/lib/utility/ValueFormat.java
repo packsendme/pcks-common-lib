ca921595bcec67ca4ad460215727c04b2bf54270
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-05-03 10:40:24 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/lib/utility/ValueFormat.java
+++ src/main/java/com/packsendme/lib/utility/ValueFormat.java
@@ -8,7 +8,7 @@ public class ValueFormat {
 	public String valueFormatToString(double value, String language, String country) {
 		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale(language,country));
 		nf.setMinimumFractionDigits(2);
-		nf.setMaximumFractionDigits(2);
+		nf.setMaximumFractionDigits(3);
 		return nf.format(value);
 	}
 }
