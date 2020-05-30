f209be181551066309cac54176c8ce421a01fb22
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-05-30 19:12:38 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/lib/utility/WeightConvert_Utility.java
+++ src/main/java/com/packsendme/lib/utility/WeightConvert_Utility.java
@@ -10,5 +10,13 @@ public class WeightConvert_Utility {
 	public Double ToneladaToGrama(double weight) {
 		return weight * 1000000;
 	}
+	
+	public Double GramaToKilograma(double weight) {
+		return weight / 1000;
+	}
+
+	public Double GramaToToneleda(double weight) {
+		return weight / 1000000;
+	}
 
 }
