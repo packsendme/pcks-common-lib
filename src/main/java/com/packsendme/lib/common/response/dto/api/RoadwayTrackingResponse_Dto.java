8f71553826b9d1aec276a6a8bc919812789af79b
ricardomarzochi@gmail.com <r.libera.marzochi@CPX-JGDMHHPGOXX.dir.svc.accenture.com>
2020-06-01 13:45:24 +0200

    NewVersion/0.2.0

--- src/main/java/com/packsendme/lib/common/response/dto/api/RoadwayTrackingResponse_Dto.java
+++ src/main/java/com/packsendme/lib/common/response/dto/api/RoadwayTrackingResponse_Dto.java
@@ -19,21 +19,22 @@ public class RoadwayTrackingResponse_Dto implements Serializable {
 	public Double toll_price;
 	public Double country_distanceF;
 	public Double country_distanceM;
-	public Double fuel_price;
+	public Double fuelGasoline_price;
+	public Double fuelDiesel_price;
 
 	public String currency_price;
 	public String unity_measurement_distance;
 	
 	public RoadwayTrackingResponse_Dto(String name_country, int toll_amount, Double toll_price, Double country_distanceF, Double country_distanceM,
-			Double fuel_price, String unity_measurement_distance) {
+			Double fuelGasoline_price, Double fuelDiesel_price) {
 		super();
 		this.name_country = name_country;
 		this.toll_amount = toll_amount;
 		this.toll_price = toll_price;
 		this.country_distanceF = country_distanceF;
 		this.country_distanceM = country_distanceM;
-		this.fuel_price = fuel_price;
-		this.unity_measurement_distance = unity_measurement_distance;
+		this.fuelGasoline_price = fuelGasoline_price;
+		this.fuelDiesel_price = fuelDiesel_price;
 	}
 	
 	
